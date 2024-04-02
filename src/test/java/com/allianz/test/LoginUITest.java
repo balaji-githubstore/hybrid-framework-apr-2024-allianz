e0becbcb6e58968c0c0da3f4f9ae9c0cfbd95848
balaji-githubstore <balaji0017@gmail.com>
2024-04-02 00:21:29 -0700

    Created two TestNG test methods

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,19 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+	
+	@Test
+	public void titleTest()
+	{
+		System.out.println("title test");
+	}
+	
+	@Test
+	public void applicationDescriptionTest()
+	{
+		System.out.println("app");
+	}
+
+}
