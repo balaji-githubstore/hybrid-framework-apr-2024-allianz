b97a482de648e03e2e64fa073ed9c1d6cd26e7df
balaji-githubstore <balaji0017@gmail.com>
2024-04-04 00:20:23 -0700

    Added browser config

---
+++ src/test/java/com/allianz/test/LoginTest.java
@@ -0,0 +1,18 @@
+package com.allianz.test;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.allianz.base.AutomationWrapper;
+
+public class LoginTest extends AutomationWrapper {
+	@Test
+	public void validLoginTest()
+	{
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		//enter password admin123
+		//click on login
+		//Assert the header - Dashboard
+	}
+
+}
--- src/test/java/com/allianz/test/LoginUITest.java
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -1,23 +1,16 @@
 package com.allianz.test;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginUITest {
+import com.allianz.base.AutomationWrapper;
+
+public class LoginUITest extends AutomationWrapper {
 	
 	@Test
 	public void titleTest()
 	{
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		
 		String actualTitle=driver.getTitle();
 		Assert.assertEquals(actualTitle, "OrangeHRM");
 	}
@@ -25,11 +18,6 @@ public class LoginUITest {
 	@Test
 	public void applicationDescriptionTest()
 	{
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		
 		String actualVersion=driver.findElement(By.xpath("//p[contains(normalize-space(),'OS')]")).getText();
 		Assert.assertEquals(actualVersion, "OrangeHRM OS 5.6.1");
 	}
