2ca0c8fbaa67da8ff2f72a9f21fac75b83f05731
balaji-githubstore <balaji0017@gmail.com>
2024-04-17 23:05:41 -0700

    Upload Profile Test Added

--- src/test/java/com/allianz/test/AddEmployeeTest.java
+++ src/test/java/com/allianz/test/AddEmployeeTest.java
@@ -1,6 +1,7 @@
 package com.allianz.test;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.allianz.base.AutomationWrapper;
@@ -14,10 +15,14 @@ public class AddEmployeeTest extends AutomationWrapper {
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 		
-		//click on PIM menu
-		//click on add employee
-		//upload the pdf file instead of image
-		//Assert the error - File type not allowed
+		driver.findElement(By.xpath("//span[text()='PIM']")).click();
+		driver.findElement(By.linkText("Add Employee")).click();
+		
+		driver.findElement(By.xpath("//input[@type='file']")).sendKeys("C:\\Automation Session\\automation_workspace\\SeleniumConcept\\XPath.txt");
+		
+		String actualError=driver.findElement(By.xpath("//span[contains(normalize-space(),'File type')]")).getText();
+		
+		Assert.assertTrue(actualError.contains("File type not allowed"));  //expect true
 	}
 
 }
